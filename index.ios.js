63adc9aaa65c8883d64528093a1eb2db675632ad
Martin Bing <bing.martin.photography@gmail.com>
2017-02-14 12:39:38 +0100

    Add webview and url to martinbing.com

--- index.ios.js
+++ index.ios.js
@@ -9,23 +9,15 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  WebView,
 } from 'react-native';
 
 export default class WebViewShell extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <WebView url={'http://www.martinbing.com'} />
       </View>
     );
   }
@@ -34,19 +26,6 @@ export default class WebViewShell extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
